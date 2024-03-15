8ab339b4871d55ac26272f73e7fcfd14deefcfcd
Ivan Ulloque <95107566+ZUR1C4T0@users.noreply.github.com>
2024-03-15 03:53:05 +0000

    Add postcss.config.js and update tailwind.config.js and app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
-import { Component } from "@angular/core";
+import { Component, type OnInit } from "@angular/core";
 import { RouterOutlet } from "@angular/router";
+import { initFlowbite } from "flowbite";
 
 @Component({
 	selector: "app-root",
@@ -7,6 +8,10 @@ import { RouterOutlet } from "@angular/router";
 	imports: [RouterOutlet],
 	templateUrl: "./app.component.html",
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
 	title = "curso-angular";
+
+	ngOnInit(): void {
+		initFlowbite();
+	}
 }
