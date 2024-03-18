235cfbffa1cbc05ffc54859078b03a975721d991
ZUR1C4T0 <ivanulloque19@gmail.com>
2024-03-18 12:48:18 -0500

    Add change detection functionality.

--- src/app/dashboard/pages/change-detection/change-detection.component.ts
+++ src/app/dashboard/pages/change-detection/change-detection.component.ts
@@ -1,11 +1,41 @@
 import { CommonModule } from "@angular/common";
-import { ChangeDetectionStrategy, Component } from "@angular/core";
+import { ChangeDetectionStrategy, Component, signal } from "@angular/core";
+import { TitleComponent } from "~/app/shared/title/title.component";
 
 @Component({
 	selector: "app-change-detection",
 	standalone: true,
-	imports: [CommonModule],
+	imports: [CommonModule, TitleComponent],
 	templateUrl: "./change-detection.component.html",
 	changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class ChangeDetectionComponent {}
+export class ChangeDetectionComponent {
+	public frameworkAsSignal = signal({
+		name: "Angular",
+		releaseYear: 2023,
+	});
+
+	public frameworkAsProperty = {
+		name: "React",
+		releaseYear: 2022,
+	};
+
+	constructor() {
+		setTimeout(() => {
+			this.changeFrameworkAsSignal();
+			this.changeFrameworkAsProperty();
+			console.log("Change detection component updated");
+		}, 3000);
+	}
+
+	public changeFrameworkAsSignal() {
+		this.frameworkAsSignal.set({
+			name: "Vue",
+			releaseYear: 2021,
+		});
+	}
+
+	public changeFrameworkAsProperty() {
+		this.frameworkAsProperty.name = "Svelte";
+	}
+}
